93731e6afa851eb97cee878ef0dcee243bb4c6ed
Cupol <107096920+Cupol@users.noreply.github.com>
2024-06-03 23:51:33 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function openModal() {
+    document.getElementById('contact-modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('contact-modal').style.display = 'none';
+}
+
+function openWhatsApp() {
+    window.open('https://wa.me/+77002903977', '_blank');
+}
+function openTelegram() {
+    window.open('https://t.me/malyshevcoach', '_blank');
+}
+
